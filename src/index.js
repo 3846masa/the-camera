37eb8edcd34185fd231767a6ca0cb8798f385e7f
3846masa <3846masahiro+git@gmail.com>
2019-02-07 21:49:53 +0900

    Add Babel

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  React.createElement('h1', {}, 'Hello World!'),
-  document.getElementById('app'),
-);
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('app'));
