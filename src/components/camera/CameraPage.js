f11c537f09ece37ad72f76f640cc32eca525c46f
3846masa <3846masahiro+git@gmail.com>
2019-02-12 18:58:09 +0900

    Get video stream

--- src/components/camera/CameraPage.js
+++ src/components/camera/CameraPage.js
@@ -4,11 +4,37 @@ import Layout from '~/components/common/Layout';
 import CameraView from '~/components/camera/CameraView';
 import CameraController from '~/components/camera/CameraController';
 
+/**
+ * @typedef State
+ * @property {MediaStream} stream
+ */
+
+/** @extends {React.Component<{}, State>} */
 class CameraPage extends React.Component {
+  /** @type {State} */
+  state = {
+    stream: null,
+  };
+
+  componentDidMount() {
+    this.updateStream();
+  }
+
+  async updateStream() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: {
+        aspectRatio: { ideal: 16 / 9 },
+        facingMode: { ideal: 'environment' },
+      },
+    });
+    this.setState({ stream });
+  }
+
   render() {
+    const { stream } = this.state;
     return (
       <Layout>
-        <CameraView />
+        <CameraView srcObject={stream} />
         <CameraController />
       </Layout>
     );
