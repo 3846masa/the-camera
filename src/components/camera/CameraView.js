36bdae9eb0c883fb7dd83a83e45217e863070962
3846masa <3846masahiro+git@gmail.com>
2019-02-14 14:05:53 +0900

    Enable to toggle facingMode

--- src/components/camera/CameraView.js
+++ src/components/camera/CameraView.js
@@ -6,16 +6,18 @@ import Video from '~/components/common/Video';
 /**
  * @typedef Props
  * @property {MediaStream} [srcObject]
+ * @property {'user' | 'environment'} facingMode
  */
 
 /** @extends {React.FC<Props>} */
-const CameraView = ({ srcObject }) => (
+const CameraView = ({ srcObject, facingMode }) => (
   <Video
     muted
     autoPlay
     playsInline
     srcObject={srcObject}
     className={styles.base}
+    data-facing-mode={facingMode}
   />
 );
 
