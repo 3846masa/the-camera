58a4084f6b0f2753be4545e72ac3bdc36c724467
3846masa <3846masahiro+git@gmail.com>
2019-02-20 21:59:09 +0900

    Add createImageBitmap polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -2,3 +2,33 @@ import { RelativeOrientationSensor } from 'motion-sensors-polyfill';
 
 window.RelativeOrientationSensor =
   window.RelativeOrientationSensor || RelativeOrientationSensor;
+
+if (!('createImageBitmap' in self)) {
+  /** @param {Blob | HTMLCanvasElement} source */
+  self.createImageBitmap = async (source) => {
+    /** @type {Blob} */
+    const blob = await new Promise((resolve) => {
+      if (source instanceof HTMLCanvasElement) {
+        source.toBlob(resolve, 'image/png');
+      } else {
+        resolve(source);
+      }
+    });
+
+    const image = new Image();
+    const waitLoadingPromise = new Promise((resolve, reject) => {
+      image.addEventListener('load', resolve, { once: true });
+      image.addEventListener('error', reject, { once: true });
+    });
+
+    const url = URL.createObjectURL(blob);
+    image.src = url;
+    await waitLoadingPromise;
+    URL.revokeObjectURL(url);
+
+    return Object.assign(image, {
+      width: image.naturalWidth,
+      height: image.naturalHeight,
+    });
+  };
+}
