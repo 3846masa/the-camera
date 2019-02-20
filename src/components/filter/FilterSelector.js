cc8be83d43f499683ab5189b67d52a09f6804620
3846masa <3846masahiro+git@gmail.com>
2019-02-20 19:39:26 +0900

    Add FilterPage

---
+++ src/components/filter/FilterSelector.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { faAdjust } from '@fortawesome/free-solid-svg-icons';
+
+import ControllerWrapper from '~/components/common/ControllerWrapper';
+import ControllerGrid from '~/components/common/ControllerGrid';
+import ControllerButton from '~/components/common/ControllerButton';
+
+/**
+ * @typedef Props
+ * @property {(filterType: string) => any} onSelect
+ * @property {string} [filterType]
+ */
+
+/** @extends {React.Component<Props>} */
+class FilterSelector extends React.Component {
+  onSelectFilter = (filterType) => {
+    const { onSelect, filterType: current } = this.props;
+
+    if (current === filterType) {
+      onSelect(null);
+    } else {
+      onSelect(filterType);
+    }
+  };
+
+  render() {
+    const { filterType } = this.props;
+
+    return (
+      <ControllerWrapper data-position="bottom">
+        <ControllerGrid>
+          <ControllerButton
+            icon={faAdjust}
+            onClick={() => this.onSelectFilter('grayscale')}
+            data-selected={filterType === 'grayscale'}
+            data-grid-area="middle"
+          />
+        </ControllerGrid>
+      </ControllerWrapper>
+    );
+  }
+}
+
+export default FilterSelector;
