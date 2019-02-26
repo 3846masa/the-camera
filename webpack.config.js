c4b6750ce6667df77b05bfffe38196207567bd10
3846masa <3846masahiro+git@gmail.com>
2019-02-27 00:08:21 +0900

    Add webmanifest

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const { LicenseWebpackPlugin } = require('license-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const WorkerPlugin = require('worker-plugin');
 const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 /** @type {import('webpack').Configuration} */
 const config = {
@@ -66,6 +67,31 @@ const config = {
     }),
     new WasmPackPlugin({
       crateDirectory: path.resolve(__dirname, './src/wasm'),
+    }),
+    new WebpackPwaManifest({
+      filename: 'manifest.webmanifest',
+      name: 'The Camera',
+      short_name: 'The Camera',
+      description: 'The Camera App works on Browser!',
+      background_color: '#000000',
+      theme_color: '#000000',
+      orientation: 'portrait',
+      display: 'standalone',
+      ios: {
+        'apple-mobile-web-app-capable': 'yes',
+        'apple-mobile-web-app-status-bar-style': 'black',
+      },
+      icons: [
+        {
+          src: path.resolve(__dirname, './src/assets/app-icon.png'),
+          sizes: [120, 152, 167, 180, 1024],
+          ios: true,
+        },
+        {
+          src: path.resolve(__dirname, './src/assets/app-icon.png'),
+          sizes: [36, 48, 72, 96, 144, 192, 512],
+        },
+      ],
     }),
   ],
   devServer: {
