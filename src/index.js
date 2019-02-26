e4d45b2f87c3fa081a17566632147c73baab7a62
3846masa <3846masahiro+git@gmail.com>
2019-02-27 01:57:04 +0900

    Enable workbox via serviceworker

--- src/index.js
+++ src/index.js
@@ -7,3 +7,9 @@ import '~/global.css';
 import App from '~/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+if ('serviceWorker' in navigator) {
+  document.addEventListener('DOMContentLoaded', () => {
+    navigator.serviceWorker.register('service-worker.js');
+  });
+}
